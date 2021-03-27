97cd94695eaf246caab3cc93cf7bcc0e86afacd8
andersencypriano <andersen.cypriano@gmail.com>
2021-03-27 17:07:29 -0300

    added methods

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,54 @@
 import '../css/style.scss';
 
+//Select element submit
 const button = document.querySelector("button[type='submit']");
 
+// Create class AutoAddress
 class AutoAddress {
-    constructor (fieldzip) {
+    constructor(fieldzip, options) {
         this.fieldzip = document.getElementById(fieldzip);
-        this.getZipCode ();
+        this.options = options;
+
+        this.getZipCode(this.fieldzip);
     }
 
-    getZipCode () {
+    //Get value of input
+    getZipCode(zipcode) {
         button.addEventListener("click", e => {
             e.preventDefault();
-            const cep = this.fieldzip.value;
+            const cep = zipcode.value;
             this.conn(cep);
         });
-    } 
-    conn (cep) {
+    };
+
+    //Request fetch and return JSON
+    conn(cep) {
         const url = `https://viacep.com.br/ws/${cep}/json/`;
         fetch(url)
-        .then(res => res.json())
-        .then(resp => console.log(resp))
-        .catch((error) => {
-            alert(`CEP: ${this.fieldzip.value} é inexistente.`);
-        });
-    }
+            .then(res => res.json())
+            .then(resp => this.createFields(resp))
+            .catch(error => {
+                console.log(error.message);
+            });
+    };
+
+    //Method from create containers input-field with params
+    createFields(data) {
+        console.log(data);
+    };
 }
-const formulario = new AutoAddress('cep');
+
+//Instance class with params
+const form1 = new AutoAddress('cep', {
+    campos: {
+        cep: true,
+        logadouro: true,
+        bairro: true,
+        localidade: true,
+        uf: true,
+        ibge: true,
+        gia: true,
+        ddd: true,
+        siafi: true,
+    }
+});
