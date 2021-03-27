10a7c3da07aecf28a3e0d09b194769fe7ba63d7b
andersencypriano <andersen.cypriano@gmail.com>
2021-03-27 14:21:46 -0300

    v0.0.2

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,30 @@
 import '../css/style.scss';
 
-console.log('Em breve =]');
+const button = document.querySelector("button[type='submit']");
+
+class AutoAddress {
+    constructor (fieldzip) {
+        this.fieldzip = document.getElementById(fieldzip);
+        this.getZipCode ();
+    }
+
+    getZipCode () {
+        button.addEventListener("click", e => {
+            e.preventDefault();
+            const cep = this.fieldzip.value;
+            this.conn(cep);
+        });
+    } 
+    conn (cep) {
+        const url = `https://viacep.com.br/ws/${cep}/json/`;
+        fetch(url)
+        .then(res => res.json())
+        .then(resp => console.log(resp))
+    }
+}
+
+
+const formulario = new AutoAddress('cep');
+
+
+
